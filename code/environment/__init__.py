e9d293a7a1f7f4793795db3f656922ebdca89cd6
Ocean Wang <37011589+OceanHWang@users.noreply.github.com>
2023-12-13 17:13:33 +0800

    formatting

--- code/environment/__init__.py
+++ code/environment/__init__.py
@@ -1,16 +1,16 @@
 '''
 This module contains classes that are used for simulating palletization.
 '''
-HEIGHT_TOLERANCE_MM = 5 # mm
+HEIGHT_TOLERANCE_MM = 5  # mm
 
-SIZE_ROLLCONTAINER = (800, 700) # mm
+SIZE_ROLLCONTAINER = (800, 700)  # mm
 '''The base area of the target "rollcontainer" in millimeters.'''
-SIZE_EURO_PALLET = (1200, 800) # mm
+SIZE_EURO_PALLET = (1200, 800)  # mm
 '''The base area of the target "euro-pallet" in millimeters.'''
-MAXHEIGHT_TARGET = 2000 # mm
+MAXHEIGHT_TARGET = 2000  # mm
 '''The maximum height of the observation space/target in millimeters.'''
 
-MAXHEIGHT_OBSERVATION_SPACE = 2000 # mm
+MAXHEIGHT_OBSERVATION_SPACE = 2000  # mm
 '''The maximum height of the observation space/target in millimeters.'''
 
 from environment.HeightMap import HeightMap
